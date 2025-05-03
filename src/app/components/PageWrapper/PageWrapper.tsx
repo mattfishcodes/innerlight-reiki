1b363513974339025b39b3b528478e1824a6ea0f
mattfisher <nuggsrocks@yahoo.com>
2025-05-03 18:50:41 -0400

    split style code

--- src/app/components/PageWrapper/PageWrapper.tsx
+++ src/app/components/PageWrapper/PageWrapper.tsx
@@ -3,13 +3,18 @@
 import { ReactNode } from 'react'
 import Header from '@/app/components/Header/Header'
 import Footer from '@/app/components/Footer/Footer'
+import { images } from '@/lib/images'
+import styles from './PageWrapper.module.scss'
 
 const PageWrapper = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <Header />
-      <div id='page-wrapper'>
-        <div id='page-background' />
+      <div className={styles.pageWrapper}>
+        <div
+          className={styles.pageBackground}
+          style={{ backgroundImage: `url('${images.mountainLakeBlue.src}')` }}
+        />
         <div className='container'>{children}</div>
       </div>
       <Footer />
