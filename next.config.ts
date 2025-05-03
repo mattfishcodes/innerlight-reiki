220705ae02ddbfd225bdeda935a9d9d80d61682b
mattfisher <nuggsrocks@yahoo.com>
2025-05-03 17:57:44 -0400

    modify nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from 'next'
 
+const command = process.env.npm_lifecycle_event
+
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.1.7'],
+  reactStrictMode: true,
+  output: command === 'build' ? 'export' : undefined,
+  trailingSlash: true,
+  allowedDevOrigins: ['192.168.1.*'],
   sassOptions: {
     silenceDeprecations: [
       'import',
