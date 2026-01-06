e3284f597acba9706f1eb3aa4a52368fca25198c
mattfisher <nuggsrocks@yahoo.com>
2026-01-05 22:52:31 -0500

    use optimize-images module

--- scripts/optimize-images.js
+++ scripts/optimize-images.js
@@ -1,39 +1,9 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
-// scripts/optimize-images.js
 
-const sharp = require('sharp')
-const fs = require('fs-extra')
 const path = require('path')
+const { optimizeImages } = require('@mattfishcodes/image-tools')
 
-const inputDir = path.join(__dirname, '../src/assets/images')
-const outputDir = path.join(__dirname, '../public/images')
-
-// You can customize these sizes
-const sizes = [16, 32, 256, 576, 768, 992, 1200, 1400]
-
-async function optimizeImages() {
-  await fs.ensureDir(outputDir)
-  const files = await fs.readdir(inputDir)
-
-  for (const file of files) {
-    const ext = path.extname(file).toLowerCase()
-    const name = path.basename(file, ext)
-
-    if (!['.jpg', '.jpeg', '.png'].includes(ext)) continue
-
-    const inputFile = path.join(inputDir, file)
-
-    for (const width of sizes) {
-      const outputFileWebP = path.join(outputDir, `${name}-${width}.webp`)
-
-      await sharp(inputFile)
-        .resize({ width })
-        .webp({ quality: 80 })
-        .toFile(outputFileWebP)
-
-      console.log(`✓ ${name}-${width}.webp`)
-    }
-  }
-}
-
-optimizeImages().catch(console.error)
+optimizeImages({
+  inputDir: path.join(__dirname, '../src/assets/images'),
+  outputDir: path.join(__dirname, '../public/images'),
+})
