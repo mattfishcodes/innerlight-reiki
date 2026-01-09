bc9a3559d9b9b81d7a4a4f3beff844545b9f3b3d
mattfisher <nuggsrocks@yahoo.com>
2026-01-09 00:57:37 -0500

    remove unnecessary modal

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -1,22 +1,10 @@
 'use client'
 
-import { useState } from 'react'
 import HeroSection from './components/HeroSection'
 import ServicesCards from './components/ServicesCards'
 import Disclaimer from './components/Disclaimer'
-import BookingModal from './components/BookingModal'
 
 const Home = () => {
-  const [showModal, setShowModal] = useState(false)
-
-  const handleClose = () => {
-    setShowModal(false)
-  }
-
-  const handleShow = () => {
-    setShowModal(true)
-  }
-
   return (
     <div className='pb-3 text-center'>
       <HeroSection />
@@ -25,15 +13,17 @@ const Home = () => {
       <div className='m-5'>
         <button
           className='bg-primary hover:bg-primary-100 cursor-pointer rounded-md p-4 text-2xl text-white transition-all hover:scale-105'
-          onClick={handleShow}
+          onClick={() =>
+            window.open(
+              'https://inner-light-reiki.moxieapp.com/public/inner-light-reiki/60-minute-in-person',
+            )
+          }
         >
           Book an Appointment Now
         </button>
       </div>
 
       <Disclaimer />
-
-      <BookingModal show={showModal} handleClose={handleClose} />
     </div>
   )
 }
