059becc7469e562d7328c57d3ec2eccd805f0a0b
mattfisher <nuggsrocks@yahoo.com>
2026-01-09 00:16:36 -0500

    improve booking modal

--- src/app/(main)/home/components/BookingModal.tsx
+++ src/app/(main)/home/components/BookingModal.tsx
@@ -2,6 +2,7 @@
 
 import { cn } from '@/lib/utils'
 import { X } from 'lucide-react'
+import { createPortal } from 'react-dom'
 
 const BookingModal = ({
   show,
@@ -10,51 +11,61 @@ const BookingModal = ({
   show: boolean
   handleClose: () => void
 }) => {
-  return (
-    <div className={cn('hidden', show && 'block')}>
-      <div className='fixed inset-0 z-30 bg-black/40' onClick={handleClose} />
+  return createPortal(
+    <>
+      <div
+        className={cn('fixed inset-0 z-30 hidden bg-black/40', show && 'block')}
+        onClick={handleClose}
+      />
       <div
         className={cn(
-          'fixed inset-x-0 top-0 z-40 bg-white',
+          'fixed top-[50%] left-[50%] z-100 -translate-1/2 bg-white',
           'origin-top transform-gpu',
           'transition-all duration-300 ease-out',
-          'pointer-events-none -translate-y-2 scale-y-95 opacity-0',
-          'flex flex-col',
+          'pointer-events-none opacity-0',
+          'flex flex-col rounded-md border border-black/50 text-left',
           'motion-reduce:transition-none',
-          show && 'pointer-events-auto translate-y-0 scale-y-100 opacity-100',
+          show && 'pointer-events-auto opacity-100',
         )}
       >
-        <header>
-          <X onClick={handleClose} />
-          <h3>Schedule an Appointment</h3>
-        </header>
-        <div>
-          <p>What type of appointment would you like to book?</p>
-          <button
-            className='hover:bg-primary-100 bg-primary mx-2 cursor-pointer rounded-md px-4 py-2 text-white transition-colors'
-            onClick={() => {
-              window.open(
-                'https://inner-light-reiki.moxieapp.com/public/inner-light-reiki/60-minute-in-person',
-              )
-              handleClose()
-            }}
-          >
-            In Person
-          </button>
-          <button
-            className='hover:bg-primary-100 bg-primary mx-2 cursor-pointer rounded-md px-4 py-2 text-white transition-colors'
-            onClick={() => {
-              window.open(
-                'https://inner-light-reiki.moxieapp.com/public/inner-light-reiki/60-minute-virtual',
-              )
-              handleClose()
-            }}
-          >
-            Virtual
+        <div className='border-b border-black/50 px-4 pt-2'>
+          <button onClick={handleClose}>
+            <X />
           </button>
         </div>
+        <div className='p-4'>
+          <header>
+            <h3>Schedule an Appointment</h3>
+          </header>
+          <div>
+            <p>What type of appointment would you like to book?</p>
+            <button
+              className='hover:bg-primary-100 bg-primary cursor-pointer rounded-md px-4 py-2 text-white transition-all hover:scale-102'
+              onClick={() => {
+                window.open(
+                  'https://inner-light-reiki.moxieapp.com/public/inner-light-reiki/60-minute-in-person',
+                )
+                handleClose()
+              }}
+            >
+              In Person
+            </button>
+            <button
+              className='hover:bg-primary-100 bg-primary ml-2 cursor-pointer rounded-md px-4 py-2 text-white transition-all hover:scale-102'
+              onClick={() => {
+                window.open(
+                  'https://inner-light-reiki.moxieapp.com/public/inner-light-reiki/60-minute-virtual',
+                )
+                handleClose()
+              }}
+            >
+              Virtual
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>,
+    document.body,
   )
 }
 
